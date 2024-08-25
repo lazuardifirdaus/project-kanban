e124064bf7a3a52f8b255ebd2feef8ee8446f828
lazuardifirdaus <lazuardi.firdaus88@gmail.com>
2024-08-25 22:46:00 +0700

    Develop add task feature

--- src/components/task-list/TaskList.tsx
+++ src/components/task-list/TaskList.tsx
@@ -1,23 +1,37 @@
 'use client'
+import { TASK_MODAL_TYPE, TASK_PROGRESS_ID } from '@/constants'
 import { tasksState } from '@/features/taskAtoms'
 import type { Task } from '@/types'
 import { useRecoilValue } from 'recoil'
 import TaskListItem from './TaskListItem'
+import TaskModal from '../TaskModal'
+import { useState } from 'react'
 
 const TaskList = (): JSX.Element => {
   const tasks: Task[] = useRecoilValue(tasksState)
-
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
   return (
     <div className="w-full p-10">
       <h1 className="text-green-500 mb-16 font-bold text-3xl">Your Tasks</h1>
       <div className="flex mb-8 gap-x-4">
-        <button className="p-4 flex items-center bg-cyan-500 text-white gap-x-2 relative">
+        <button
+          className="p-4 flex items-center bg-cyan-500 text-white gap-x-2 relative"
+          onClick={(): void => setIsModalOpen(true)}
+        >
           <span className="material-icons">add</span>Add task
         </button>
         <button className="p-4 flex items-center bg-cyan-500 text-white gap-x-2 relative">
           <span className="material-icons">sort</span>Filter tasks
         </button>
       </div>
+      {isModalOpen && (
+        <TaskModal
+          headingTitle="Add your task"
+          type={TASK_MODAL_TYPE.ADD}
+          setIsModalOpen={setIsModalOpen}
+          defaultProgressOrder={TASK_PROGRESS_ID.NOT_STARTED}
+        />
+      )}
       <div>
         <div className="flex text-2xl border-b border-b-gray-300 *:p-4">
           <div className="w-1/4">Task Name</div>
--- src/components/task-progress/TaskColumn.tsx
+++ src/components/task-progress/TaskColumn.tsx
@@ -1,5 +1,10 @@
+import { TASK_MODAL_TYPE, TASK_PROGRESS_ID } from '@/constants'
+import { useState } from 'react'
 import TaskCard from './TaskCard'
 import type { Task } from '@/types'
+import TaskModal from '../TaskModal'
+import { tasksState } from '@/features/taskAtoms'
+// import { useRecoilValue } from 'recoil'
 
 interface TaskColumnProps {
   columnTitle: string
@@ -8,12 +13,24 @@ interface TaskColumnProps {
 }
 
 const TaskColumn = ({ columnTitle, tasks, columnId }: TaskColumnProps): JSX.Element => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
+
   return (
     <div className="max-w-[400px]">
       <div className="flex justify-between items-center p-1">
         <h2 className="font-bold text-xl">{columnTitle}</h2>
-        <div className="material-icons cursor-pointer">add</div>
+        <div className="material-icons cursor-pointer" onClick={(): void => setIsModalOpen(true)}>
+          add
+        </div>
       </div>
+      {isModalOpen && (
+        <TaskModal
+          headingTitle="Add your task"
+          type={TASK_MODAL_TYPE.ADD}
+          setIsModalOpen={setIsModalOpen}
+          defaultProgressOrder={columnId}
+        />
+      )}
       <div>
         {tasks.map((task: Task) => {
           return <TaskCard key={task.id} task={task} />
